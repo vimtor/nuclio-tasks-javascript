0a23f7ddce42c43f860b1c0e89af649c448e59e4
Victor Navarro <victor@vimtor.io>
2021-03-19 21:07:04 +0100

    Basic project finished

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 const input = document.querySelector("input");
 const form = document.querySelector("form");
 const list = document.querySelector("ul");
+const error = document.querySelector(".error");
 
-function addItem(newValue) {
+function getInitialTasks() {
+  const storedTasks = localStorage.getItem("tasks");
+  if (storedTasks) {
+    return storedTasks.split(",");
+  }
+  return [];
+}
+
+function createItem(title) {
   const item = document.createElement("li");
-  item.innerText = newValue;
+  item.innerText = title;
+  item.addEventListener("click", () => {
+    list.removeChild(item);
+    tasks = tasks.filter((task) => task !== title);
+    localStorage.setItem("tasks", tasks);
+  });
+
   list.append(item);
 }
 
@@ -13,10 +28,31 @@ function clearInput() {
   input.focus();
 }
 
+function setError(message) {
+  error.innerText = message;
+}
+
+function clearError() {
+  error.innerText = "";
+}
+
+function saveTask(title) {
+  tasks.push(title);
+  localStorage.setItem("tasks", tasks);
+}
+
+let tasks = getInitialTasks();
+
+tasks.forEach(createItem);
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   if (input.value) {
-    addItem(input.value);
+    createItem(input.value);
+    saveTask(input.value);
     clearInput();
+    clearError();
+  } else {
+    setError("Your task cannot be empty");
   }
 });
